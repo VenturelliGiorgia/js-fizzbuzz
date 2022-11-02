dfb349b74ceb26551caae17f9792419e5bb53cae
VenturelliGiorgia <venturelli.giorgia.03@gmail.com>
2022-11-02 17:08:15 +0100

    bonus 1

--- js/main.js
+++ js/main.js
@@ -1,17 +1,24 @@
+const output = document.querySelector("div.output");
 for (let i = 0; i < 100; i++) {
 
 
     if (i % 3 === 0 & i % 5 === 0) { //se i è divisibile per 3 e 5 scrivi FizzBuzz
         console.log("FizzBuzz");
+        output.append(" FizzBuzz ");
     }
     else if (i % 5 === 0) { //se i è divisibile per 5 scrivi Buzz
         console.log("Buzz");
+        output.append(" Buzz ");
     }
     else if (i % 3 === 0) { //se i è divisibile per 3 scrivi Fizz
         console.log("Fizz");
+        output.append(" Fizz ");
     }
     else {
         console.log(i);
+        output.append(" ");
+        output.append(i);
+        output.append(" ");
     }
 
 }
